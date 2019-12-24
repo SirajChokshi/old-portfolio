5941d677214ade2259d00c131285d4852f77d72b
Siraj <sirajchokshi@gmail.com>
2019-12-23 20:00:43 -0500

    add build

---
+++ build/precache-manifest.059ab2b62c3cb4a21b13b6565aebd8de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d785674e427fd5ea307e53bc4653779f",
+    "url": "/new-portfolio/index.html"
+  },
+  {
+    "revision": "1e96983ab2665a0d2949",
+    "url": "/new-portfolio/static/css/main.aa50807f.chunk.css"
+  },
+  {
+    "revision": "73c19d8ba7a070a75735",
+    "url": "/new-portfolio/static/js/2.2538550c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/new-portfolio/static/js/2.2538550c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1e96983ab2665a0d2949",
+    "url": "/new-portfolio/static/js/main.34222694.chunk.js"
+  },
+  {
+    "revision": "19819cbd7899edc84298",
+    "url": "/new-portfolio/static/js/runtime-main.e5d55734.js"
+  }
+]);
