d34721f8b6eef157277f97b6b096d9df5164774f
Siraj <sirajchokshi@gmail.com>
2020-02-12 09:22:59 -0600

    oxford comma

--- src/views/About.js
+++ src/views/About.js
@@ -23,10 +23,10 @@ export default class About extends Component {
                         <div className="columns eight" id="about-page-bio">
                             <p>
                                 <strong>Hi, I'm Siraj.</strong><br /> I am a user experience designer and frontend developer with foundations
-                                in application development, user research and visual design. I am enrolled at the University
+                                in application development, user research, and visual design. I am enrolled at the University
                                 of Illinois at Urbana-Champaign where I am pursuing a bachelors degree in Cognitive Science
                                 and minoring in Art + Design. My coursework is centered around human factors, ergonomics,
-                                programming and design. I am currently designing and developing solutions for the UIUC
+                                programming, and design. I am currently designing and developing solutions for the UIUC
                                 startup community at <a href="http://founders.illinois.edu" target="_blank" rel='noreferrer noopener' className="box-link">
                                 Founders<i style={{fontSize: "1rem", marginLeft: "5px"}} className="fas fa-external-link-alt" /></a> and for non-profit organizations at <a href="https://uiuc.hack4impact.org" target="_blank" rel='noreferrer noopener' className="box-link">
                                 Hack4Impact<i style={{fontSize: "1rem", marginLeft: "5px"}} className="fas fa-external-link-alt" /></a> .
