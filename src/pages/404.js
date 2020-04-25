996e64078afc93746ee3fbb5b77c85a62b001d03
Siraj <sirajchokshi@gmail.com>
2020-04-25 13:47:52 -0400

    remove formatting from 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,13 +7,7 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
-  <Layout>
   <Redirect noThrow to={`/`} />
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p style={{fontSize: "20px"}}>This page was likely deleted or moved elsewhere. Sorry for the inconvenience!</p>
-      <Link to="/" className="button btn">Return Home</Link>
-  </Layout>
 );
 
 export default NotFoundPage
