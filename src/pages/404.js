5574fb907447fb566f2ce741dd2d3640d56c5482
Siraj <sirajchokshi@gmail.com>
2020-05-30 12:29:26 -0400

    only redirect on 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,12 +5,7 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
-  <>
   <Redirect noThrow to="/" />
-  <Layout>
-    <SEO title="404: Not found" />
-  </Layout>
-  </>
 )
 
 export default NotFoundPage
