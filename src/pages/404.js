67d4fa077ab76fd30f9593c6bca86a76e168f03c
Siraj <sirajchokshi@gmail.com>
2020-05-30 12:27:13 -0400

    fix 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -9,10 +9,6 @@ const NotFoundPage = () => (
   <Redirect noThrow to="/" />
   <Layout>
     <SEO title="404: Not found" />
-    <h2 className="work-header page-title">404 - NOT FOUND</h2>
-    <p style={{lineHeight: 2}}>The content you were looking for has likely moved :( <br />
-      <Link to="/">Return home</Link> or <a href="https://archive.sirajchokshi.com/">check the archives</a>
-    </p>
   </Layout>
   </>
 )
